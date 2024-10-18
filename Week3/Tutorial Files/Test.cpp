7b48987a95075f4a9385923506a28ef42aaf268a
YJR117 <133361144+EdricYeo117@users.noreply.github.com>
2024-10-19 02:54:25 +0800

    Completed week 3 practical

---
+++ Week3/Tutorial Files/Test.cpp
@@ -0,0 +1,6 @@
+//(i) create a Node object and set node2 to point to it
+node2 = new Node;
+//(ii) store “Vivian” in the Node object pointed to by node2. 
+node2->name = "Vivian";
+//(iii)  initialize the next pointer in Node object, pointed to by node2, to NULL
+node2->next = NULL;
